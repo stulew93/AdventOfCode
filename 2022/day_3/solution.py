ed265d343b2d5f451dff72fbf22c73e634f4d268
Stuart Lewis <stuart.lewis3110@gmail.com>
2022-12-11 19:52:20 +0000

    Create common function for creating daily directories

---
+++ 2022/day_3/solution.py
@@ -0,0 +1,5 @@
+from common_functions import ingest_csv_to_list
+
+if __name__ == "__main__":
+    filepath = "input.csv"
+    input_list = ingest_csv_to_list(filepath)
--- common_functions.py
+++ common_functions.py
@@ -1,3 +1,8 @@
+import os
+import shutil
+import subprocess
+
+
 def ingest_csv_to_list(filepath: str) -> list:
     result_list = []
     with open(filepath, 'r') as input_file:
@@ -5,3 +10,19 @@ def ingest_csv_to_list(filepath: str) -> list:
             result_list.append(str(row.strip()))
 
     return result_list
+
+
+def create_new_day(dir_name: str) -> None:
+    # os.mkdir(dir_name)
+    open(os.path.join(dir_name, "input.csv"), 'a').close()
+    # open(os.path.join(dir_name, "solution.py"), 'a').close()
+    shutil.copyfile("solution_template.py", os.path.join(dir_name, "solution.py"))
+    # os.system("cd " + dir_name)
+    os.system(f"git add {dir_name}/input.csv")
+    os.system(f"git add {dir_name}/solution.py")
+    return
+
+
+if __name__ == "__main__":
+    day_directory = "2022/day_3"
+    create_new_day(day_directory)
